e22692e6c49bd9cfba410d4b0d753a10f2304bab
wpaholic <wpaholic@gmail.com>
2026-01-14 20:13:34 -0500

    add docs

---
+++ docs/build/q-CP5sUfbc.js
@@ -0,0 +1,1 @@
+import{h as r}from"./q-CPbSDhbW.js";import{_hW as n}from"./q-CPbSDhbW.js";const m=o=>{const[t]=r();if(!t.submitted)return t.submit(o)};export{n as _hW,m as s_GZVmeWA7SYM};
---
+++ docs/build/q-DWM2tbiL.js
@@ -0,0 +1,1 @@
+import{h as t}from"./q-CPbSDhbW.js";import{_hW as f}from"./q-CPbSDhbW.js";const s=()=>{const[r,e]=t();return e(r)};export{f as _hW,s as s_kXsQvLiHc2g};
